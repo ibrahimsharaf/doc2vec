14376860b84999acef94c09cf25ca927e4c3c09c
Aya Amr <ayatallah.amr@gmail.com>
2019-05-14 01:37:19 +0200

    #14 added doc2vec class and constructor

--- models/doc2vec_builder.py
+++ models/doc2vec_builder.py
@@ -3,3 +3,12 @@ import random
 import numpy as np
 from gensim.models import doc2vec
 from gensim.models.doc2vec import Doc2Vec
+
+from .model_builder import ModelBuilder
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+
+class doc2VecBuilder(ModelBuilder):
+
+    def __init__(self, corpus):
+        self.corpus = corpus
