1658426970dde70b44a6098a5bc2e0e631990bbd
Aya Amr <ayatallah.amr@gmail.com>
2019-05-14 01:53:20 +0200

    #14 added classifier class abstract method train_model

--- models/classifier_builder.py
+++ models/classifier_builder.py
@@ -1,6 +1,31 @@
+import logging
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, f1_score
+
 from .model_builder import ModelBuilder
 from .doc2vec_builder import doc2VecBuilder
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 class classifierBuilder(ModelBuilder):
     def __init__(self):
         super.__init__()
+
+    def initialize_model(self):
+        self.model = LogisticRegression()
+
+    def train_model(self, d2v, training_vectors, training_labels):
+        logging.info("Classifier training")
+        train_vectors = doc2VecBuilder.get_vectors(d2v, len(training_vectors), 300, 'Train')
+        self.model.fit(train_vectors, np.array(training_labels))
+        training_predictions = self.model.predict(train_vectors)
+        logging.info('Training predicted classes: {}'.format(np.unique(training_predictions)))
+        logging.info('Training accuracy: {}'.format(accuracy_score(training_labels, training_predictions)))
+        logging.info('Training F1 score: {}'.format(f1_score(training_labels, training_predictions, average='weighted')))
+
+
+    def save_model(self):
+        pass
+
+    def load_model(self):
+        pass
