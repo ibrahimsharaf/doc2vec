67d6ee9bc5033a5a8dbe3509376bee4fd1e7104b
Aya Amr <ayatallah.amr@gmail.com>
2019-05-14 02:01:30 +0200

    #14 added classifier class rest of abstract methods

--- models/classifier_builder.py
+++ models/classifier_builder.py
@@ -1,8 +1,9 @@
 import logging
 import numpy as np
+import os
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, f1_score
-
+from sklearn.externals import joblib
 from .model_builder import ModelBuilder
 from .doc2vec_builder import doc2VecBuilder
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
@@ -24,8 +25,12 @@ class classifierBuilder(ModelBuilder):
         logging.info('Training F1 score: {}'.format(f1_score(training_labels, training_predictions, average='weighted')))
 
 
-    def save_model(self):
-        pass
+    def save_model(self, filename):
+        joblib.dump(self.model,"./classifiers/"+ filename)
 
-    def load_model(self):
-        pass
+    def load_model(self,filename):
+        if (os.path.isfile('./classifiers/' + filename)):
+            loaded_model = joblib.load(filename)
+            self.model = loaded_model
+        else:
+            self.model = None
